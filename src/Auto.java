d76f01fff59cd235e676acba38d216d6e0b3c736
NurmaProduct <98742536+NurmaShone@users.noreply.github.com>
2022-11-08 19:46:56 +0600

    swsw

---
+++ src/Auto.java
@@ -0,0 +1,16 @@
+public class Auto {
+    String auto;
+
+    public Auto(String auto) {
+        this.auto = auto;
+        System.out.println(auto);
+    }
+
+    public String getAuto() {
+        return auto;
+    }
+
+    public void setAuto(String auto) {
+        this.auto = auto;
+    }
+}
