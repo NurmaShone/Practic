794960a13750de04481b990f8a0eca06febd9ecf
NurmaProduct <98742536+NurmaShone@users.noreply.github.com>
2022-11-08 20:09:41 +0600

    swsw

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("hi");
         Auto auto = new Auto("bmw");
         System.out.println("your car is bmw");
-
+        System.out.println("это новая ветка с моим именем");
     }
 }
